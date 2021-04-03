53bc7ca7c5c11129875999df9ef709587fc99c30
ClaytonJK <77551168+ClaytonJK@users.noreply.github.com>
2021-04-03 17:36:27 -0400

    Add files via upload

--- AccelerometerProject.py
+++ AccelerometerProject.py
@@ -3,12 +3,16 @@ import board
 import busio
 import digitalio
 import adafruit_lis3dh
+import pyudev
 
 print("Starting Program")
 
+context = pyudev.Context()
 i2c = busio.I2C(board.SCL, board.SDA)
 lis3dh = adafruit_lis3dh.LIS3DH_I2C(i2c)
 
+for device in context.list_devices():
+    device
 if lis3dh.shake()==True:
     print("Shaken")
 else:
